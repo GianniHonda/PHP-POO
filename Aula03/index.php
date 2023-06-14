d3d6240ac9a3b4836cfab677b8e0d8d98863fbfd
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-06-14 19:05:14 +0900

    Visibilidade de atributos e métodos PHP POO
    Configurando a visibilidade dos atributos e métodos do objeto criado no exercício anterior.

---
+++ Aula03/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula 02 - POO</title>
+    </head>
+    <body>
+        <?php
+        require_once 'Caneta.php';
+        $c1 = new Caneta;
+        $c1->cor = "Azul";
+        $c1->ponta = 0.5;
+        $c1->tampar();
+        print_r($c1);
+        echo '<br/>';
+        $c2 = new Caneta;
+        $c2->cor = "Verde";
+        $c2->carga = 50;
+        $c2->tampar();
+        print_r($c2);
+        
+        ?>
+    </body>
+</html>
