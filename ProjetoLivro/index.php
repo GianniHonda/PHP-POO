045029d32995d30dd05aa826d72ca9366c1f381f
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-09-11 10:58:46 +0900

    Book project
    Another encapsulation practice where people read books, with an interface addendum.

---
+++ ProjetoLivro/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <pre>
+        <?php
+        require_once 'Pessoa.php';
+        require_once 'Livro.php';
+        $p[0] = new Pessoa('Pedro', 22, 'M');
+        $p[1] = new Pessoa('Maria', 31, 'F');
+        
+        $l[0] = new Livro('PHP Básico', 'José da Silva', 300, $p[0]);
+        $l[1] = new Livro('POO com PHP', 'Maria de Souza', 500, $p[0]);
+        $l[2] = new Livro('PHP avançado', 'Ana Paula', 800, $p[1]);
+        
+        $l[0]->abrir();
+        $l[0]->folhear(80);
+        $l[0]->avancarPag();
+        $l[0]->detalhes();
+        
+        $l[1]->detalhes();
+        
+        $l[2]->detalhes();
+        ?></pre>
+    </body>
+</html>
